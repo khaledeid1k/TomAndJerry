cf2e289b5ef4dae23a09f51e734709f2d446b058
Khaled.Eid <khaled.eid1k1k@gmail.com>
2025-05-25 05:25:52 +0200

    Add IBM Plex Sans Arabic font

--- app/src/main/java/com/example/tomandjerry/ui/theme/Type.kt
+++ app/src/main/java/com/example/tomandjerry/ui/theme/Type.kt
@@ -2,10 +2,21 @@ package com.example.tomandjerry.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.tomandjerry.R
 
+val ibmPlexSans = FontFamily(
+    Font(R.font.ibm_plex_sans_arabic_regular, FontWeight.Normal),
+    Font(R.font.ibm_plex_sans_arabic_bold, FontWeight.Bold),
+    Font(R.font.ibm_plex_sans_arabic_light, FontWeight.Light),
+    Font(R.font.ibm_plex_sans_arabic_medium, FontWeight.Medium),
+    Font(R.font.ibm_plex_sans_arabic_semi_bold, FontWeight.SemiBold),
+    Font(R.font.ibm_plex_sans_arabic_thin, FontWeight.Thin),
+    Font(R.font.ibm_plex_sans_arabic_extra_light, FontWeight.ExtraLight)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
