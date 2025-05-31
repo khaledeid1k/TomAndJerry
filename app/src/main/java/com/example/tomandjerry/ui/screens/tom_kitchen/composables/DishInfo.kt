161f24baf26ef1fa95474bf2b6a2bcf9cf4ac787
Khaled.Eid <khaled.eid1k1k@gmail.com>
2025-05-31 15:46:39 +0200

    Add dish description text

--- app/src/main/java/com/example/tomandjerry/ui/screens/tom_kitchen/composables/DishInfo.kt
+++ app/src/main/java/com/example/tomandjerry/ui/screens/tom_kitchen/composables/DishInfo.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,34 +19,46 @@ import com.example.tomandjerry.R
 import com.example.tomandjerry.ui.composables.CheesesItem
 import com.example.tomandjerry.ui.composables.VerticalSpacer
 import com.example.tomandjerry.ui.theme.EerieBlack
+import com.example.tomandjerry.ui.theme.darkOverlay
 import com.example.tomandjerry.ui.theme.ibmPlexSans
 
 @Composable
 fun DishInfo(modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Column {
-            Text(
-                text = "Electric Tom pasta",
-
-                fontSize = 20.sp,
-                fontWeight = FontWeight.SemiBold,
-                fontFamily = ibmPlexSans,
-                color = EerieBlack
-            )
-            VerticalSpacer(space = 12.dp)
-            CheesesItem(price = "5", modifier = Modifier)
-        }
-        IconButton(
-            onClick = { },
+    Column {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.favorit),
-                contentDescription = null
-            )
+            Column {
+                Text(
+                    text = "Electric Tom pasta",
+
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = ibmPlexSans,
+                    color = EerieBlack
+                )
+                VerticalSpacer(space = 12.dp)
+                CheesesItem(price = "5", modifier = Modifier)
+            }
+            IconButton(
+                onClick = { },
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.favorit),
+                    contentDescription = null
+                )
+            }
         }
+
+        Text(
+            "Pasta cooked with a charger cable and sprinkled with questionable cheese. Make sure to unplug it before eating (or not, you decide).",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            fontFamily = ibmPlexSans,
+            color = darkOverlay,
+            modifier = Modifier.padding(top = 16.dp)
+        )
     }
 }
