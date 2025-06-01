ed0c8bac40f95a80f3e51a275702e4d45d5bdb5d
Khaled.Eid <khaled.eid1k1k@gmail.com>
2025-06-01 03:24:15 +0200

    Call SecretEpisodesScreen in TomAndJerryApp

--- app/src/main/java/com/example/tomandjerry/TomAndJerryApp.kt
+++ app/src/main/java/com/example/tomandjerry/TomAndJerryApp.kt
@@ -5,18 +5,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.tomandjerry.ui.screens.home.HomeScreen
-import com.example.tomandjerry.ui.screens.tom_kitchen.TomKitchenScreen
+import com.example.tomandjerry.ui.screens.SecretEpisodes.SecretEpisodesScreen
 import com.example.tomandjerry.ui.theme.TomAndJerryTheme
 
 @Composable
 fun TomAndJerryApp(modifier: Modifier = Modifier) {
     TomAndJerryTheme {
         Scaffold(
-            topBar = {},
-            modifier = Modifier.fillMaxSize()) { innerPadding ->
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            // HomeScreen(modifier = modifier.padding(innerPadding))
+            //  TomKitchenScreen(modifier = modifier.padding(innerPadding))
+            // AccountScreen(modifier = modifier.padding(innerPadding))
+            SecretEpisodesScreen(modifier = modifier.padding(innerPadding))
 
-            TomKitchenScreen(modifier = modifier.padding(innerPadding))
 
         }
     }
